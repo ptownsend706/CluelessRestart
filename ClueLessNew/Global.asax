10f73f6344e42d9ceb09bd0386c00a2d26d1deba
Phylicia Townsend <pfleetw@yahoo.com>
2019-04-28 21:37:20 -0400

    Commit staged

---
+++ ClueLessNew/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClueLessNew.Global" Language="C#" %>
---
+++ ClueLessNew/Site.Master.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClueLessNew
+{
+    public partial class SiteMaster : MasterPage
+    {
+        string playerName = "";
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PlayerTextBox_TextChanged(object sender, EventArgs e)
+        {
+            playerName = PlayerTextBox.Text;
+        }
+
+        protected void JoinGameButton_Click(object sender, EventArgs e)
+        {
+            
+            if (playerName == "")
+            {
+                ErrorLabel.Text = "Please enter your name.";
+            }
+            else if (playerName != "")
+            {
+                ErrorLabel.Text = "";
+            }
+        }
+
+        protected void gameLogTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
